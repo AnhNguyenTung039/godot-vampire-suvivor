3d3b67fa5a02b3b2bf1f4c100a8745c42a807c2d
Nguyen Tung Anh <nguyentunganh039@gmail.com>
2024-07-15 22:34:28 +0700

    update on main character

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,3 +5,8 @@ func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600 # Move to the direction with speed of 600 pixcel/sec
 	move_and_slide()
+	
+	if velocity.length() > 0.0 :
+		%HappyBoo.play_walk_animation()
+	else :
+		%HappyBoo.play_idle_animation()
