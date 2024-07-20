46c469528ca01536cb03ad4d51f27c38ae788b24
Jin Kisara <nguyentunganh039@gmail.com>
2024-07-21 01:43:13 +0700

    Add weapon and damage function

--- mob.gd
+++ mob.gd
@@ -1,8 +1,16 @@
 extends CharacterBody2D
 
+var health = 3
+
 @onready var player = %Player
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 300.0
 	move_and_slide()
+
+func take_damage():
+	health -= 1
+	
+	if health == 0:
+		queue_free()
